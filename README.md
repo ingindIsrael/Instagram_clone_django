b5acc611f45a943f090e097497004c731323d0b4
ingindIsrael <ing.visrael@gmail.com>
2021-07-20 15:53:55 -0400

    global var secret aws

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 pip install -r requirements.txt
 python manage.py runserver
 python manage.py collectstatic --noinput
+DISABLE_COLLECTSTATIC=1
--- platzigram/settings.py
+++ platzigram/settings.py
@@ -161,9 +161,15 @@ _locals = locals()
 
 django_heroku.settings(_locals)
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+
+print(AWS_ACCESS_KEY_ID)
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 
-# AWS_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
+AWS_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
 
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
